bc8fa6fea1be40baefa3993126c37c8cd2099159
Martin De Lojo <martinmdl42@gmail.com>
2025-08-06 09:10:20 -0300

    ejercicio 1 terminado

--- 01_carreraCaballos.py
+++ 01_carreraCaballos.py
@@ -1,22 +1,27 @@
-from threading import Thread
+from threading import Thread, Lock
 from time import sleep
 
 gano = False
+lock = Lock()
 
 def caballo(num):
     global gano
     for i in range(1, 4):
-        if gano:
-            return
         print(f"Corre {num}")
         sleep(2)
-    gano = True
-    print(f"El caballo {num} ganó la carrera")
+        with lock:
+            if gano:
+                return
+        
+    with lock:
+        if not gano:
+            gano = True
+            print(f"El caballo {num} llegó a la meta")
 
 threads = []
 
-for i in range(1, 5):
-    thread = Thread(target=caballo(i))
+for i in range(1, 11):
+    thread = Thread(target=caballo, args=(i,))
     threads.append(thread)
     
 for thread in threads:
